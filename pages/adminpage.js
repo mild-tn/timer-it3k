2ad68c852f584e69a5794bc4d620d47cc8c68700
Mild-TN <mild20091931@gmail.com>
2019-02-01 03:20:58 +0700

    [ADD] adminpage

---
+++ pages/adminpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Admin from '../components/Admin/index'
+
+class Index extends React.Component {
+  render () {
+    return (
+      <Admin/>
+    )
+  }
+}
+
+export default Index
