46482f34ace32197bde053b559275e6060d61f48
Mild-TN <mild20091931@gmail.com>
2019-02-01 03:21:38 +0700

    [ADD] config

---
+++ config/colors.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const colors = {
+    color1 : '#fff',
+    darkSalmon : '#E09F7D',
+    sunsetOrange : '#EF5D60',
+    paradisePink : '#EC4067',
+    darkPurple : '#A01A7D',
+    blackcurrant : '#311847',
+    color2 : '#EF5D60',
+    color3 : '#E9B6B7',
+    violet : '#311847',
+    voteBtn : '#A01A7D;',
+    backgroundnew: 'linear-gradient(150.6deg, #F3BBAB 0%, #C49AAB 100%)'
+}
+
+export default colors;
---
+++ config/fonts.js
@@ -0,0 +1,13 @@
+export default {
+  IT3K:'100px',
+  number: '280px',
+  web: '72px',
+  headline: '36px',
+  title: '20px',
+  subtitle: '18px',
+  paragraph: '16px',
+  body: '14px',
+  caption: '12px',
+  subcaption: '10px',
+  timeout : '150px'
+}
